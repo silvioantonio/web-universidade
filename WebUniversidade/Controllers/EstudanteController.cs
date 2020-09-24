90fb0d947c218f6c148fee7ac3e22cb09eaf617f
Silvio Antônio de Oliveira Júnior <silvio.junior@crp.com>
2020-09-24 14:52:31 -0300

    Update page

--- WebUniversidade/Controllers/EstudanteController.cs
+++ WebUniversidade/Controllers/EstudanteController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebUniversidade.Data;
+using WebUniversidade.Models;
 
 namespace WebUniversidade.Controllers
 {
@@ -18,11 +19,14 @@ namespace WebUniversidade.Controllers
             this.Contexto = contexto;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             return View(await Contexto.Estudantes.ToListAsync());
         }
 
+        [HttpGet]
+        [Route("Detalhes/{id:int}")]
         public async Task<IActionResult> Detalhes(int? id)
         {
             if (id == null)
@@ -39,6 +43,74 @@ namespace WebUniversidade.Controllers
 
             return View(estudante);
         }
+
+        [HttpGet]
+        [Route("Editar/{id:int}")]
+        public async Task<IActionResult> Editar(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound(new { message = "Codigo do estudante nao cadastrado no banco de dados" });
+            }
+            var estudante = await Contexto.Estudantes.FindAsync(id);
+
+            if (estudante == null)
+            {
+                return NotFound(new { message = "Estudante nao cadastrado no banco de dados" });
+            }
+            return View(estudante);
+        }
+
+        [HttpPost]
+        [Route("Editar/{id:int}")]
+        public async Task<IActionResult> Editar(int? id, [Bind("Id, EnrollmentDate, Nome, Sobrenome")] Estudante estudante)
+        {
+            if (id != estudante.Id)
+                return NotFound(new { message = "Id do estudante não esta na base de dados!" });
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    Contexto.Update(estudante);
+                    await Contexto.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException)
+                {
+                    ModelState.AddModelError("", "Nao foi possivel salvar os dados");
+                }
+            }
+            return View(estudante);
+        }
+
+        [HttpGet]
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Criar(Estudante estudante)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    Contexto.Add(estudante);
+                    await Contexto.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (DbUpdateException db)
+            {
+                ModelState.AddModelError("", "Não foi possivel salvar os dados, Tente novamente");
+            }
+            return View(estudante);
+        }
+
+        
 
     }
 }
