cc2d967518c77b046b4312fa832cf1ed2abd0d40
Silvio Antônio de Oliveira Júnior <silvio.junior@crp.com>
2020-09-24 09:46:41 -0300

    Personaliza a pagina de detalhes do estudante

--- WebUniversidade/Controllers/EstudanteController.cs
+++ WebUniversidade/Controllers/EstudanteController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -20,6 +21,23 @@ namespace WebUniversidade.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await Contexto.Estudantes.ToListAsync());
+        }
+
+        public async Task<IActionResult> Detalhes(int? id)
+        {
+            if (id == null)
+                return NotFound(new { message = "Codigo passado não encontrado na base de dados!" });
+
+            var estudante = await Contexto.Estudantes
+                .Include(e => e.CursoEstudantes)
+                .ThenInclude(e => e.Curso)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (estudante == null)
+                return NotFound(new { message = "Estudante não encontrado na base de dados!" });
+
+            return View(estudante);
         }
 
     }
