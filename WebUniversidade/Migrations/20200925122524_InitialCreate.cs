cec0164c116f8d48319b550cbc3ec5192f06f6f9
Silvio Antônio de Oliveira Júnior <silvio.junior@crp.com>
2020-09-25 09:35:41 -0300

    Adicionando migração ao projeto utilizando EF Core migrations

---
+++ WebUniversidade/Migrations/20200925122524_InitialCreate.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebUniversidade.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Curso",
+                columns: table => new
+                {
+                    CursoId = table.Column<int>(nullable: false),
+                    Titulo = table.Column<string>(nullable: true),
+                    Creditos = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Curso", x => x.CursoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Estudante",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(nullable: true),
+                    Sobrenome = table.Column<string>(nullable: true),
+                    EnrollmentDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estudante", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CursoEstudante",
+                columns: table => new
+                {
+                    CursoID = table.Column<int>(nullable: false),
+                    EstudanteId = table.Column<int>(nullable: false),
+                    CursoEstudateId = table.Column<int>(nullable: false),
+                    Grade = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CursoEstudante", x => new { x.CursoID, x.EstudanteId });
+                    table.ForeignKey(
+                        name: "FK_CursoEstudante_Curso_CursoID",
+                        column: x => x.CursoID,
+                        principalTable: "Curso",
+                        principalColumn: "CursoId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CursoEstudante_Estudante_EstudanteId",
+                        column: x => x.EstudanteId,
+                        principalTable: "Estudante",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CursoEstudante_EstudanteId",
+                table: "CursoEstudante",
+                column: "EstudanteId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CursoEstudante");
+
+            migrationBuilder.DropTable(
+                name: "Curso");
+
+            migrationBuilder.DropTable(
+                name: "Estudante");
+        }
+    }
+}
