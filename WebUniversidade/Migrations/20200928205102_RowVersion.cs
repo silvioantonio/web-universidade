4582714f28fc8f8c286b70ee9116cf8a766518d0
Silvio Antônio de Oliveira Júnior <silvio.junior@crp.com>
2020-09-28 17:55:03 -0300

    Migracao inclusao de controlador de versao de dados

---
+++ WebUniversidade/Migrations/20200928205102_RowVersion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebUniversidade.Migrations
+{
+    public partial class RowVersion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "Departamento",
+                rowVersion: true,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "Departamento");
+        }
+    }
+}
